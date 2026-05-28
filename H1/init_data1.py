52667b40e41876e15040969fcf1642e87bd73e92
guest <forschslv@gmail.com>
2026-05-28 15:04:53 +0300

    создал начало обработки данных

---
+++ H1/init_data1.py
@@ -0,0 +1,54 @@
+import os, re
+
+import pandas as pd
+from bs4 import BeautifulSoup
+def get_data_by_time(time):
+    data = pd.DataFrame(columns=['place', 'name', 'from', 'class', '1', '2', '3', '4', 'sum'])
+
+    with open(f"Материалы/Первый тур/{time}.html") as f:
+        text = f.read()
+    soup = BeautifulSoup(text, 'html.parser')
+    skip = 2
+    for i in soup.find_all(name='tr'):
+        if skip:
+            skip -= 1
+            continue
+        # print(i.text)
+        print(i.prettify())
+        # print(soup.prettify(str(i), encoding='utf-8'))
+        try:
+            name_from_class = i.find(name='td', attrs={'class': "party"}).text
+        except Exception as e:
+            print('\033[93m'+repr(e)+'\033[0m')
+            continue
+
+        name = ' '.join(name_from_class.split()[:3])
+        from_ = name_from_class[name_from_class.find('(') + 1: name_from_class.find(',')]
+        class_ = name_from_class[name_from_class.find(',') + 1: name_from_class.find('класс')]
+
+
+
+        # print(row)
+        # break
+        zad = []
+        sm = 0
+        for j in i.find_all(name = 'td', attrs={'class': 'ioiprob'}):
+            if j.text.strip() == '.':
+                zad.append(0)
+            else:
+                zad.append(int(j.text))
+                sm += zad[-1]
+        row = {'place': [i.find(name='td', attrs={'class': "rankl"}).text],
+               'name': [name],
+               'from': [from_],
+               'class': [class_],
+               '1': [zad[0]],
+               '2': [zad[1]],
+               '3': [zad[2]],
+               '4': [zad[3]],
+               'sum': [sm]}
+        print(row)
+        data = pd.concat([data,
+                          pd.DataFrame(row)],
+                         axis=0)
+    return data
